bb42d384b09cc80768a394a7193e30d3e9ca6c0b
Ricky Zheng <rickyzheng2@yahoo.com>
2021-03-18 12:52:10 -0400

    added routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route, BrowserRouter as Router, Switch, Link } from "react-router-dom";
+import Home from './routes/Home';
+import Learn from './routes/Learn';
+import Signin from "./routes/Signin";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="container">
+        <Link className="navbar-item" to="/">
+          <h1>AlgoStreet</h1>
+        </Link>
+        <Link className="navbar-item" to="/learn">
+          <div>Learn</div>
+        </Link>
+        <Link className="navbar-item" to="/signin">
+          <div>Sign-in</div>
+        </Link>
+      </div>
+
+      <div className="container mt-4">
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/learn" exact component={Learn} />
+          <Route path="/signin" exact component={Signin} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/navbar.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function navbar(): React.ReactNode {
+    return (
+        <div>
+            AlgoStreet
+        </div>
+    )
+}
